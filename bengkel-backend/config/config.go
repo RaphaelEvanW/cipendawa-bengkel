f0a2c0935e27a61b66fc4e5e31c781fa55c4b0e7
RaphaelEvanW <raphael.wijayanto@gmail.com>
2026-06-10 17:09:55 +0700

    init: project structure + go dependencies

---
+++ bengkel-backend/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	JWTSecret  string
+	Port       string
+}
+
+func Load() *Config {
+	godotenv.Load()
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "bengkel_db"),
+		JWTSecret:  getEnv("JWT_SECRET", "secret"),
+		Port:       getEnv("PORT", "8080"),
+	}
+}
+
+func (c *Config) DBConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName,
+	)
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
