b696ba2490e64410771efc9d9956da14ccd2d45d
RaphaelEvanW <raphael.wijayanto@gmail.com>
2026-06-11 17:13:23 +0700

    change main backend

--- bengkel-backend/cmd/main.go
+++ bengkel-backend/cmd/main.go
@@ -5,6 +5,8 @@ import (
 
 	"bengkel-backend/config"
 	"bengkel-backend/internal/auth"
+	"bengkel-backend/internal/schedule"
+	"bengkel-backend/internal/service"
 	"bengkel-backend/pkg"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +18,15 @@ func main() {
 
 	authService := auth.NewService(db, cfg)
 	authHandler := auth.NewHandler(authService)
+
+	// inisialisasi service module
+	serviceRepo := service.NewRepository(db)
+	serviceService := service.NewService(serviceRepo)
+	serviceHandler := service.NewHandler(serviceService)
+
+	scheduleRepo := schedule.NewRepository(db)
+	scheduleService := schedule.NewService(scheduleRepo)
+	scheduleHandler := schedule.NewHandler(scheduleService)
 
 	r := gin.Default()
 
@@ -29,10 +40,27 @@ func main() {
 		{
 			authRoutes.POST("/login", authHandler.Login)
 		}
+		//Routes buat public
+		api.GET("/services", serviceHandler.GetAll)
+		api.GET("/service/:id", serviceHandler.GetByID)
+		api.GET("/schedule/available", scheduleHandler.GetAvailable)
 
+		// Routes buat admin
 		adminRoutes := api.Group("/admin")
 		adminRoutes.Use(auth.AuthMiddleware(cfg))
 		{
+			//Service routes
+			adminRoutes.GET("/services", serviceHandler.GetAllAdmin)
+			adminRoutes.POST("/services", serviceHandler.Create)
+			adminRoutes.PUT("/services:id", serviceHandler.Update)
+			adminRoutes.DELETE("/services/:id", serviceHandler.Delete)
+
+			//Schedule routes
+			adminRoutes.GET("/schedule", scheduleHandler.GetAll)
+			adminRoutes.GET("/schedule/:id", scheduleHandler.GetByID)
+			adminRoutes.POST("/schedule", scheduleHandler.Create)
+			adminRoutes.PATCH("/schedule/:id", scheduleHandler.Update)
+			adminRoutes.DELETE("/schedule/:id", scheduleHandler.Delete)
 		}
 	}
 
